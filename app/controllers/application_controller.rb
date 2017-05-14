c0e1078362f6d84badd6cdedbf552d1bfdbcfe03
Nelly Duclos <knopfler42@yahoo.fr>
2017-05-14 23:24:42 +0200

    added exception for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: "facturation@keycoopt.com", password: "superstrongpassword"
+  http_basic_authenticate_with name: "facturation@keycoopt.com", password: "superstrongpassword" ,except: :create
 end
