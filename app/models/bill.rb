d2730f28856fb6007a2eaad09670b23a90898cf6
Nelly Duclos <knopfler42@yahoo.fr>
2017-05-15 12:45:27 +0200

    Fixed the unique reference generator

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,7 +1,7 @@
 class Bill < ApplicationRecord
   belongs_to :publication
 
-  before_save :reference_generator
+  before_save :unique_refference #:reference_generator
   before_save :add_price
 
   require "i18n"
@@ -9,29 +9,28 @@ class Bill < ApplicationRecord
 
   validates :reference, uniqueness: true
 
-  def reference_generator
-    unless self.reference.present?
-      self.reference = "#" + customer_ref + generate_reference_number
+
+  def unique_refference
+    if send(:read_attribute, :reference) == self.reference
+      reference_generator
     else
-      uniqueness_of_reference
+      reference_generator
     end
+  end
+
+  def reference_generator
+    self.reference = "#" + customer_ref + generate_reference_number
   end
 
   def customer_ref
     ref_1 = I18n.transliterate(self.publication.customer).gsub(/\W/, '')
-    # I18n.transliterate removes all accents but keeps letters à => a
     ref_1.first(4).upcase
+    # I18n.transliterate removes all accents but keeps letters à => a
   end
 
   def generate_reference_number
     rand.to_s[4..7]
     #that generate a random number with 4 to 7 digits
-  end
-
-  def uniqueness_of_reference
-    if self.reference == reference_generator
-      generate_reference_number
-    end
   end
 
 
