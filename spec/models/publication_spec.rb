632511a51d37ae89195b9ed3736776b26bd90939
Nelly Duclos <knopfler42@yahoo.fr>
2017-05-17 13:15:36 +0200

    callback test

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe Publication  do
   it { should have_one(:bill)}
   it { should validate_uniqueness_of(:customer).scoped_to(:title) }
+  it { is_expected.to callback(:alert).after(:create) }
+
+
 end
 
 
